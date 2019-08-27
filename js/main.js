e1a08b2c7e46005f88309d5ecd0f49760aed3260
Alex <o.a.momotyuk@gmail.com>
2019-08-27 14:48:21 -0500

    My Pet This - done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*
+/*  Practice: Represent your Pet
     Create an object that represents your pet.
 
     Name property with a string value.
@@ -6,14 +6,23 @@
     Nicknames property with an array value. Array contains strings.
     Age property with an integer value
 */
-
+/*  Practice: Pet Sounds
+    Now that you have an object that represents your pet, it's time to add some behavior. Pick three behaviors that your pet has and add three kys to your pet object. Each key's value should be a function that logs to the console or displays a window alert. Your choice.
+*/
+/*  Practice: My Pet This
+    Give your pet a new key named favoriteToys whose value is an empty array.
+    Now define another new key named play whose value is a function with a single parameter named toy.
+    The function should determine if your pet liked the toy, and if s/he does, then it should be added to the favoriteToys array using the this keyword
+*/
 let pet =  {
     name: "parrot",
     species: "bird",
     nickname: "Cockatoo",
     age: 5,
 
-    flying: function fly ( where ) {
+    favoriteToys: [ ],
+
+    flying: function ( where ) {
         window.alert( this.name + " " + this.nickname + " is flying high in a " + where + "." );
     },
 
@@ -23,9 +32,27 @@ let pet =  {
 
     singing: ( what ) => {
         window.alert( this.name + " " + this.nickname + " is singing a " + what + "." );
+    },
+/* It doesn't work: 
+    play: ( toy ) => {
+        if ( toy === "mirror" ) { this.favoriteToys.push( toy ); }
+        console.log( "Cockatoo favorite toys are ", this.favoriteToys );
     }
+*/
+    play: function ( toy ) {
+        if ( toy.indexOf( "mirror" ) != -1 ) { this.favoriteToys.push( toy ); }
+        console.log( "Cockatoo favorite toys are ", this.favoriteToys );
+    }
+
+
+
 }
 
 pet.flying( "sky" );
 pet.playing( "mirror" );
 pet.singing( "song" );
+
+pet.play( "round mirror" );
+pet.play( "mirror ball" );
+pet.play( "square mirror" );
+pet.play( "hard candy");
